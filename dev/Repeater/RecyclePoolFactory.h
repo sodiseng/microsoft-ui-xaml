3ffb584f85c2abbd95b7537f6527fc737e35835b
sodiseng <sodisenggm@gmail.com>
2023-11-04 18:30:32 +0800

    update to 2.8.3

---
+++ dev/Repeater/RecyclePoolFactory.h
@@ -0,0 +1,10 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+#pragma once
+
+#include "RecyclePool.h"
+
+#include "RecyclePool.properties.cpp"
+
+
